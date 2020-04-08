03d02bda9d45ee61ebc6a83abbc0e0001c79335f
paulodelia <paulohdelia@gmail.com>
2020-04-08 11:27:21 -0300

    feat: add filter and pagination

--- src/app/models/Instructor.js
+++ src/app/models/Instructor.js
@@ -60,8 +60,8 @@ module.exports = {
                 SELECT instructors.*, COUNT(members) AS total_students
                 FROM instructors
                 LEFT JOIN members ON (instructors.id = members.instructor_id)
-                WHERE instructors.name ILIKE '${filter}%'
-                OR instructors.services ILIKE '${filter}%'
+                WHERE instructors.name ILIKE '%${filter}%'
+                OR instructors.services ILIKE '%${filter}%'
                 GROUP BY instructors.id
                 ORDER BY total_students DESC
             `
@@ -105,5 +105,41 @@ module.exports = {
             
             callback();
         });
+    },
+
+    paginate(params) {
+        const { filter, limit, offset, callback } = params
+        
+        let query = "",
+            filterQuery = "",
+            totalQuery = `(
+                SELECT count(*) FROM instructors
+            ) AS total`
+
+        if( filter ) {
+            filterQuery = `
+                WHERE instructors.name ILIKE '%${filter}%'
+                OR instructors.services ILIKE '%${filter}%'
+            `
+
+            totalQuery = `(
+                SELECT count(*) FROM instructors
+                ${filterQuery}
+            ) as total`
+        }
+
+        query = `
+            SELECT instructors.*, ${totalQuery} , count(members) as total_students
+            FROM instructors
+            LEFT JOIN members ON (instructors.id = members.instructor_id)
+            ${filterQuery}
+            GROUP BY instructors.id LIMIT $1 OFFSET $2
+        `
+
+        db.query(query, [limit, offset], function(err, results){
+            if (err) throw `Database Error! ${err}`;
+            
+            callback(results.rows);
+        })
     }
 }
